0b0cfe910dd76482b0819ca047119904475b27af
Christian Nagel <christian@christiannagel.com>
2017-08-16 16:19:52 +0200

    async and services

---
+++ AsyncSample/AsyncSample/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Threading.Tasks;
+
+namespace AsyncSample
+{
+    [Activity(Label = "AsyncSample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            FindViewById<Button>(Resource.Id.button1).Click += async (sender, e) =>
+            {
+
+
+                await Task.Delay(3000).ConfigureAwait(false);
+
+                RunOnUiThread(() =>
+                {
+                    FindViewById<TextView>(Resource.Id.textView1).Text = "after the delay";
+                });
+
+
+            };
+
+
+        }
+
+    }
+}
+
