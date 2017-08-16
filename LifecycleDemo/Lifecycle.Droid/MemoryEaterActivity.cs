2d750218b17e5f36224e4f2d6e045a00eb9c8c57
Christian Nagel <christian@christiannagel.com>
2017-08-16 11:09:11 +0200

    lifecyle

---
+++ LifecycleDemo/Lifecycle.Droid/MemoryEaterActivity.cs
@@ -0,0 +1,26 @@
+using Android.App;
+using Android.OS;
+using Android.Util;
+
+
+namespace Lifecycle.Droid
+{
+    [Activity (Label = "Memory Eater")]			
+	public class MemoryEaterActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			Log.Debug("MemoryEater", "MemoryEater Activity launched, now consuming memory");
+		}
+
+		protected override void OnStart ()
+		{
+			base.OnStart ();
+
+			// consume memory
+			int[] Big = new int[100000000];
+		}
+	}
+}
+
