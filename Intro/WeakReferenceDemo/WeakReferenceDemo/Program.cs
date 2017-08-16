252cd34d00cee496d3d16825977b1d398423947d
Christian Nagel <christian@christiannagel.com>
2017-08-16 12:59:11 +0200

    weak reference demo

--- Intro/WeakReferenceDemo/WeakReferenceDemo/Program.cs
+++ Intro/WeakReferenceDemo/WeakReferenceDemo/Program.cs
@@ -7,6 +7,20 @@ namespace WeakReferenceDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            object o1 = new object();
+
+            WeakReference wr = new WeakReference(o1);
+            o1 = null;
+
+            GC.Collect(1);
+
+
+            object o2 = wr.Target;
+            if (o2 != null)
+            {
+
+            }
         }
     }
 }
